23400831d5a5b124941cc4835c7d017d0695bb90
misterJS <64055552+misterJS@users.noreply.github.com>
2024-09-17 04:45:12 +0700

    add new web

--- src/components/portfolio/SinglePortfolioV1.jsx
+++ src/components/portfolio/SinglePortfolioV1.jsx
@@ -1,30 +1,40 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const SinglePortfolioV1 = ({ portfolio }) => {
-    const { id, thumb, tag, title, icon } = portfolio
+  const { id, thumb, tag, title, icon } = portfolio;
 
-    return (
-        <>
-            <div className="portfolio-style-one animate" data-animate="fadeInUp">
-                <div className="thumb-zoom">
-                    <Link href={`/project-details/${id}`}>
-                        <Image src={`/assets/img/projects/${thumb}`} width={800} height={800} alt="Image Not Found" />
-                    </Link>
-                </div>
-                <div className="pf-item-info">
-                    <div className="content-info">
-                        <span>{tag}</span>
-                        <h2><Link href={`/project-details/${id}`}>{title}</Link></h2>
-                    </div>
-                    <div className="button">
-                        <Link href={`/project-details/${id}`} className="pf-btn"><i className={icon}></i></Link>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="portfolio-style-one animate" data-animate="fadeInUp">
+        <div className="thumb-zoom">
+          {/* /project-details/${id} */}
+          <Link href={`#`}>
+            <Image
+              src={`/assets/img/projects/${thumb}`}
+              width={800}
+              height={800}
+              alt="Image Not Found"
+            />
+          </Link>
+        </div>
+        <div className="pf-item-info">
+          <div className="content-info">
+            <span>{tag}</span>
+            <h2>
+              <Link href={`#`}>{title}</Link>
+            </h2>
+          </div>
+          <div className="button">
+            <Link href={`#`} className="pf-btn">
+              <i className={icon}></i>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SinglePortfolioV1;
