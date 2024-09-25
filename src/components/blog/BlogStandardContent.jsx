ace65689106fb8e3505f590f218a15b07f99388a
misterJS <64055552+misterJS@users.noreply.github.com>
2024-09-25 14:25:12 +0700

    fix error deployment

--- src/components/blog/BlogStandardContent.jsx
+++ src/components/blog/BlogStandardContent.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React from 'react';
 import blogData from '@/assets/jsonData/blog/BlogData.json'
 import Link from 'next/link';
--- src/components/header/SidebarInfo.jsx
+++ src/components/header/SidebarInfo.jsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from 'next/image';
 import React from 'react';
 import logoLight from '@/assets/img/logo-light.png';
